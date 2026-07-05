920666effcffc421f29694b804a0606f5f149722
Ayan Abbas <ayanabbas727@gmail.com>
2026-07-05 13:16:08 +0530

    Creating user profile API's

--- backend/app/api/router.py
+++ backend/app/api/router.py
@@ -2,7 +2,9 @@ from fastapi import APIRouter
 
 from app.api.v1.companies import router as companies_router
 from app.api.v1.health import router as health_router
+from app.api.v1.profiles import router as profiles_router
 
 api_router = APIRouter()
 api_router.include_router(health_router, tags=["health"])
 api_router.include_router(companies_router)
+api_router.include_router(profiles_router)
